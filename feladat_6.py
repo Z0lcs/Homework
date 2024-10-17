9a538ee1de7f7b420b260cb762856ceefa0be594
Arany Zoltán <aranyzoli11@gmail.com>
2024-10-17 21:43:51 +0200

    feladat_6 javítva

--- feladat_6.py
+++ feladat_6.py
@@ -1,5 +1,12 @@
 import random
 veletlen_szam = random.randint(1, 12)
+numbers_szama = 0
+harom_szam = 0
 
-if veletlen_szam % 3 == 0:
-    print(veletlen_szam)
+while numbers_szama != 20:
+    veletlen_szam = random.randint(1, 12)
+    numbers_szama += 1
+    if veletlen_szam % 3 == 0:
+        print(veletlen_szam)
+        harom_szam += 1
+print(str(harom_szam) + " darab szám osztható 3-mal!")
